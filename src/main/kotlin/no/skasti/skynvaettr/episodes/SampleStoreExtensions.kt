a14a3008b78a44b9da3e2ebc0a665082a56244ff
Stig-Rune <stig-rune.skansgard@bekk.no>
2026-09-09 15:01:13 +0200

    fix: use vararg SignalId for episode materialization

--- src/main/kotlin/no/skasti/skynvaettr/episodes/SampleStoreExtensions.kt
+++ src/main/kotlin/no/skasti/skynvaettr/episodes/SampleStoreExtensions.kt
@@ -18,7 +18,7 @@ fun SampleStore.get(episode: EpisodeDefinition): EpisodeData {
     val samples = get(
         episode.from,
         episode.to,
-        signalIds,
+        *signalIds.toTypedArray(),
     )
 
     val grouped = samples.groupBy { it.timestamp }
