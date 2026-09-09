e39d6d2ab16596d58b97630ad4dc1715f2717b93
Stig-Rune <stig-rune.skansgard@bekk.no>
2026-09-09 15:00:54 +0200

    refactor: make SignalId a regular value object

--- src/main/kotlin/no/skasti/skynvaettr/signals/SignalId.kt
+++ src/main/kotlin/no/skasti/skynvaettr/signals/SignalId.kt
@@ -3,8 +3,7 @@ package no.skasti.skynvaettr.signals
 /**
  * Stable identity of a [Signal].
  */
-@JvmInline
-value class SignalId(val value: String) {
+data class SignalId(val value: String) {
     init {
         require(value.isNotBlank()) { "Signal id must not be blank" }
     }
