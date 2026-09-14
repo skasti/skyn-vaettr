c00019df3b4299aef88d3f3a07eb33f824782016
Stig-Rune <stig-rune.skansgard@bekk.no>
2026-09-14 11:44:27 +0200

    Preserve Vaettr graph shorthand constructor

--- src/main/kotlin/no/skasti/skynvaettr/Vaettr.kt
+++ src/main/kotlin/no/skasti/skynvaettr/Vaettr.kt
@@ -23,6 +23,8 @@ class Vaettr(
 ) {
     private val graph: ProcessingGraph = graph ?: SensingProcessingGraph(sampleStore)
 
+    constructor(graph: ProcessingGraph) : this(InMemorySampleStore(), graph, emptyList())
+
     fun sense(samples: List<Sample<*>>) {
         if (samples.isEmpty()) return
 
